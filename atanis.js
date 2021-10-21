527f8bd319167bb28e394526b797222d9c8314b3
Glitch (atahan-selfbot) <none>
2021-10-21 17:34:47 +0000

    📟✂️ Checkpoint ./atanis.js:28725120/576 ./events/ready.js:28725120/76

--- atanis.js
+++ atanis.js
@@ -169,7 +169,8 @@ let sebep = db.fetch(`afk_sebep`)
 if (!afk) return
 if (afk === "Açık") {
 if (message.content === `${client.user}`) {
-message.reply(`${client.user} Şu anda ${sebep} Sebebinden AFK'dır lütfen rahatsız etmeyiniz.`)
+if (message.author.bot === true) return
+message.reply(new MessageEmbed().setDescription(`${client.user} Şu anda ${sebep} Sebebinden AFK'dır lütfen rahatsız etmeyiniz.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('#3498db').setFooter(`Atahan SelfBot`).setTimestamp()).then(x => x.delete({timeout: 10000}));
 }
 }})
 
--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,11 @@
 const Discord = require('discord.js-selfbot');
+const db = require('quick.db');
 const ayarlar = require('../ayarlar.json');
 
 module.exports = client => {
   
   let durum = ayarlar.durum
+  let sebep = db.fetch(`afk_sebep`)
 client.user.setPresence({ activity: { name: durum }, status: "dnd" }) 
 
 }
