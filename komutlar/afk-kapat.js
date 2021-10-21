409715875d7d3de3e1b89f87c8fb1f11cde1c738
Glitch (atahan-selfbot) <none>
2021-10-21 17:00:27 +0000

    🐔🍮 Checkpoint ./komutlar/afk-kapat.js:28725120/449 ./komutlar/afk.js:28725120/607

--- komutlar/afk-kapat.js
+++ komutlar/afk-kapat.js
@@ -2,10 +2,11 @@ const { MessageEmbed } = require("discord.js-selfbot");
 const db = require("quick.db");
 const ayarlar = require("../ayarlar.json");
 let basarili = ayarlar.basariliemoji;
+let basarisiz = ayarlar.basarisizemoji
 exports.run = async (client, message, args) => {
   
   if (message.author.id !== ayarlar.sahip) return
-  if (!db.fetch(`afk`)) return
+  if (!db.fetch(`afk`)) return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Görünüşe göre afk modu zaten kapalı`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
   
 message.channel.send(new MessageEmbed().setDescription(`${basarili} ${message.author}, Başarıyla afk modu kapandı.`).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x348f36').setTimestamp()).then(x => x.delete({timeout: 5000}));
 db.delete(`afk`)
@@ -20,7 +21,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: "afk-kapat",
+  name: "unafk",
   description: "Afk Olmanızı Sağlar.",
   usage: "afk / afk <sebep>"
 };
--- komutlar/afk.js
+++ komutlar/afk.js
@@ -2,14 +2,15 @@ const { MessageEmbed } = require("discord.js-selfbot");
 const db = require("quick.db");
 const ayarlar = require("../ayarlar.json");
 let basarili = ayarlar.basariliemoji;
+let basarisiz = ayarlar.basarisizemoji
 exports.run = async (client, message, args) => {
   
   if (message.author.id !== ayarlar.sahip) return
   
   const sebep = args[0];
+  
+  if (db.fetch(`afk`)) return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Görünüşe göre afk modu zaten kapalı`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
   if (!args[0]) {
-    let kullanıcı = message.guild.members.cache.get(message.author.id);
-    const b = kullanıcı.displayName;
 
     await db.set(
       `afk_sebep`,
@@ -21,11 +22,10 @@ exports.run = async (client, message, args) => {
       `afk_sebep`
     );
 
-   message.channel.send(new MessageEmbed().setColor('BLACK').setDescription(`${basarili} ${kullanıcı} Başarıyla Afk Oldunuz Afk Olmanızın Sebebi: **${ramo}**`));
+   message.channel.send(new MessageEmbed().setColor('BLACK').setDescription(`${basarili} ${message.author}, Başarıyla Afk Oldunuz Afk Olmanızın Sebebi: **${ramo}**`));
   }
   if (args[0]) {
     let sebep = args.join(" ");
-    let kullanıcı = message.guild.members.cache.get(message.author.id);
     await db.set(`afk_sebep`, sebep);
     await db.set(`afk`)
     
@@ -33,7 +33,7 @@ exports.run = async (client, message, args) => {
       `afk_sebep`
     );
 
-   message.channel.send(new MessageEmbed().setColor('BLACK').setDescription(`${basarili} ${kullanıcı} Başarıyla Afk Oldunuz Afk Olmanızın Sebebi: **${ramo}**`));
+   message.channel.send(new MessageEmbed().setColor('BLACK').setDescription(`${basarili} ${message.author}, Başarıyla Afk Oldunuz Afk Olmanızın Sebebi: **${ramo}**`));
 
   }
 };
