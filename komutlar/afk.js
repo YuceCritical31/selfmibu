42f5b9589d9687168c7cd6f27e1a69ed9abb8f66
Glitch (atahan-selfbot) <none>
2021-10-23 19:49:57 +0000

    🚈📒 Checkpoint ./komutlar/afk.js:28725120/33

--- komutlar/afk.js
+++ komutlar/afk.js
@@ -7,7 +7,7 @@ exports.run = async (client, message, args) => {
   
   if (message.author.id !== ayarlar.sahip) return
   if (db.fetch(`afk`) === "Açık") return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Görünüşe göre afk modu zaten açık.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
-let sebep = args[0]
+let sebep = args.slice(0).join(' ');
 if (!sebep) return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Bir sebep belirtmelisin.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
   
 message.channel.send(new MessageEmbed().setDescription(`${basarili} ${message.author}, Başarıyla \`${sebep}\` sebebiyle afk oldunuz.`).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x348f36').setTimestamp()).then(x => x.delete({timeout: 5000}));
