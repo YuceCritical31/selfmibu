2f8e7f4bb774d454e6e664f63fab6f77a5b4906b
Glitch (atahan-selfbot) <none>
2021-10-24 12:26:23 +0000

    🔖🌞 Checkpoint ./atanis.js:28725120/75 ./events/ready.js:28725120/117

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,4 @@ const ayarlar = require('../ayarlar.json');
 
 module.exports = client => {
   
-  let durum = db.fetch(`durum`) || ayarlar.durum
-client.user.setPresence({ activity: { name: durum }, status: "dnd"}) 
-}
+  }
