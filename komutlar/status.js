fce92439f0fc94c452cf31785ab5b0781ed689ef
Glitch (atahan-selfbot) <none>
2021-10-24 14:35:26 +0000

    ⏱🎠 Checkpoint ./ayarlar.json:28725120/36 ./events/ready.js:28725120/24 ./komutlar/status.js:28725120/300

--- komutlar/status.js
+++ komutlar/status.js
@@ -9,22 +9,26 @@ if (message.author.id !== ayarlar.sahip)
 return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Komutu kullanmak için yetkin bulunmamakta.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
 
 let data = db.fetch(`status`)
+let data2 = db.fetch(`statusdata`)
 let status = args[0]
 if(!status) return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Lütfen durumunuzu belirtiniz.\n1 = Çevrimiçi\n2 = Boşta\n3 = Rahatsız Etmeyin`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp())
-if (data === status) return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Durumunuz önceki ile aynı olamaz.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp())
+if (data === data2) return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Durumunuz önceki ile aynı olamaz.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp())
 
 if (status === "1"){
 db.set(`status`, "online")
-args[0] = "Çevrimiçi"
+db.set(`statusdata`, "online")
+status = "Çevrimiçi"
 }
   
 if (status === "2"){
 db.set(`status`, "idle")
-args[0] = "Boşta"
+db.set(`statusdata`, "idle")
+status = "Boşta"
 }
 
 if (status === "3"){
 db.set(`status`, "dnd")
+db.set(`statusdata`, "dnd")
 args[0] = "Rahatsız Etmeyin"
 }
 
