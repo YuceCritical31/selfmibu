21ea6e2e3a2a351200e84ae65004583cca7f3c0c
Glitch (atahan-selfbot) <none>
2021-10-24 00:59:35 +0000

    🐜😼 Checkpoint ./komutlar/yardım.js:28725120/1611

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -5,6 +5,7 @@ const db = require("quick.db")
 
 exports.run = async(client, message, args) => {
   
+  if (message.author.id !== ayarlar.sahip) return
 
     let prefix = db.fetch(`prefix`) || ayarlar.prefix
     let basarili = ayarlar.basariliemoji;
@@ -17,7 +18,16 @@ const extacy = new Discord.MessageEmbed()
 .setFooter('Atahan Tarafından Yapılmıştır.')
 .setThumbnail(message.author.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
 .setDescription(`
-> **\`${prefix}ses Aç/Kapat -> Ses sitemini \`** 
+> **\`${prefix}ses Aç/Kapat -> Ses sitemini Açar/Kapatır\`**
+> **\`${prefix}ses-kanal <#(ses kanal id)> -> Aktif oluncak ses kanalını ayarlar\`**
+> **\`${prefix}afk <sebep> -> Sizi afk moduna sokar\`**
+> **\`${prefix}unafk -> Sizi afk modundan çıkarır\`**
+> **\`${prefix}durum <durum> -> Durumunuzu değişir\`**
+> **\`${prefix}durum-sifirla -> Durumunuzu sıfırlar\`**
+> **\`${prefix}prefix <prefix> -> Prefixinizi değişir\`**
+> **\`${prefix}ping -> Botun pingini gösterir\`**
+> **\`${prefix}restart -> Botu yeniden başlatır\`**
+> **\`.unuttum -> Prefixi unuttuysanız bu komut ile prefixi görebilirsiniz\`**
 `)
 message.channel.send(extacy)
 
@@ -26,7 +36,7 @@ exports.conf = {
 	enabled : true,
 	guildOnly : false,
 	aliases : ['help','yardim'],
-	permLevel : 0
+	permLevel : 4
 }
 
 exports.help = {
