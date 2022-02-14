813abe7605741de0451d34792702edd39152e050
Glitch (atahan-selfbot) <none>
2022-02-14 10:16:28 +0000

    🚠📹 Checkpoint ./komutlar/durum.js:28725120/441 ./komutlar/status.js:28725120/795

--- komutlar/durum.js
+++ komutlar/durum.js
@@ -6,12 +6,12 @@ let basarisiz = ayarlar.basarisizemoji;
 exports.run = async (bot, message, args) => {
   
 if (message.author.id !== ayarlar.sahip)
-return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Komutu kullanmak için yetkin bulunmamakta.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+return message.channel.send(`${basarisiz} ${message.author}, Komutu kullanmak için yetkin bulunmamakta.`).setColor('0x800d0d').then(x => x.delete({timeout: 5000}));
 
 let data = db.fetch(`durum`)
 let durum = args.splice(0).join(" ")
-if(!durum) return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Lütfen durumunuzu belirtiniz.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp())
-if (data === durum) return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Durumunuz önceki ile aynı olamaz.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp())
+if(!durum) return message.channel.send(`${basarisiz} ${message.author}, Lütfen durumunuzu belirtiniz.`)
+if (data === durum) return message.channel.send(`${basarisiz} ${message.author}, Durumunuz önceki ile aynı olamaz.`)
   
 message.channel.send(`${basari} ${message.author}, Durumunuz \`${durum}\` olarak ayarlandı.`)
 
--- komutlar/status.js
+++ komutlar/status.js
@@ -6,12 +6,14 @@ let basarisiz = ayarlar.basarisizemoji;
 exports.run = async (bot, message, args) => {
   
 if (message.author.id !== ayarlar.sahip)
-return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Komutu kullanmak için yetkin bulunmamakta.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+return message.channel.send(`${basarisiz} ${message.author}, Komutu kullanmak için yetkin bulunmamakta.`).then(x => x.delete({timeout: 5000}));
 
+let sayılar = ["1","2","3","4"]
 let data = db.fetch(`status`)
 let status = args[0]
-if(!status) return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Lütfen durumunuzu belirtiniz.\n1 = Çevrimiçi\n2 = Boşta\n3 = Rahatsız Etmeyin\n4 = Görünmez`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp())
-
+if(!status) return message.channel.send(`${basarisiz} ${message.author}, Lütfen durumunuzu belirtiniz.\n1 = Çevrimiçi\n2 = Boşta\n3 = Rahatsız Etmeyin\n4 = Görünmez`)
+if(!sayılar.some(word => message.content.includes(word))) return message.channel.send(`${basarisiz} ${message.author}, Lütfen durumunuzu belirtiniz.\n1 = Çevrimiçi\n2 = Boşta\n3 = Rahatsız Etmeyin\n4 = Görünmez`)
+  
 if (status === "1"){
 db.set(`status`, "online")
 status = "Çevrimiçi"
@@ -32,7 +34,7 @@ db.set(`status`, "invisible")
 status = "Görünmez"
 }
 
-message.channel.send(new MessageEmbed().setDescription(`${basari} ${message.author}, Durumunuz \`${status}\` olarak ayarlandı.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x348f36').setTimestamp())
+message.channel.send(`${basari} ${message.author}, Durumunuz \`${status}\` olarak ayarlandı.`)
 message.react('✅')
 };
 
