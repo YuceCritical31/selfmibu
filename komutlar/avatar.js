1e6a679d26406246a596c6d4cc862fb81748ca5d
Glitch (atahan-selfbot) <none>
2022-02-16 00:22:56 +0000

    🎺💅 Checkpoint ./komutlar/avatar.js:28725120/2514

--- komutlar/avatar.js
+++ komutlar/avatar.js
@@ -1,31 +1,19 @@
-const Discord = require('discord.js-selfbot')
+const Discord = require('discord.js')
 const ayarlar = require("../ayarlar.json");
 const db = require('quick.db')
 
 
-exports.run = function(client, message, args) {
-  
-if (message.author.id !== ayarlar.sahip) return
+exports.run = async (client, message, args) {
          
-let basarisiz = ayarlar.basarisizemoji;
-let basari = ayarlar.basariliemoji;
-let muser = message.mentions.users.first()
-let user = message.mentions.users.first() || client.users.fetch(userid);
-
-let userid;
-if(isNaN(args[0])){
-  if(!muser){
-    userid = message.author.id;
-  }else{
-    userid = muser.id;
-  }
-}else{
-  userid = args[0];
-}
-if(!user) {user = message.author}
+  let basarisiz = ayarlar.basarisizemoji;
   
-message.channel.send(user.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
-message.react('✅')
+const embed = message.mentions.users.first() || client.users.fetch(args[0]);
+let user;
+if (message.mentions.users.first())  {user = message.mentions.users.first();}
+  
+else {user = message.author;}
+
+return message.channel.send(embed.avatarURL({ dynamic: true, size: 1024 }))
 };
 
 exports.conf = {
