7f8527367d9e6970a9beeaa52a24e6ae836fa91d
Glitch (atahan-selfbot) <none>
2022-02-16 20:07:37 +0000

    ☕️🏆 Checkpoint ./komutlar/sil.js:28725120/31

--- komutlar/sil.js
+++ komutlar/sil.js
@@ -13,13 +13,12 @@ let member = message.author
 
 if(!args[0]) return message.channel.send(`${basarisiz} ${message.author}, Bir sayı belirtmelisin!`)
   
-var i = 0;
+var i = args[0];
 message.delete();
 channel.messages.fetch().then(x => {
 x.filter(a => a.author.id === member.id).map(a => a).slice(args[0]).forEach(s => {
-i++
 s.delete();
-if(i === x.filter(a => a.author.id === member.id).map(a => a).slice(0, 100).length) {
+if(i === x.filter(a => a.author.id === member.id).map(a => a).slice(args[0]).length) {
 return message.channel.send(`${basari} ${message.author}, **${i}** Mesaj siliniyor.`);
 }
 });
