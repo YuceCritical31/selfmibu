2d5fc02964f72801693a76fc4565303ce03bb125
Glitch (atahan-selfbot) <none>
2022-02-16 00:58:01 +0000

    🚏😜 Checkpoint ./komutlar/avatar.js:28725120/3161

--- komutlar/avatar.js
+++ komutlar/avatar.js
@@ -1,31 +1,54 @@
-const Discord = require('discord.js-selfbot')
-const ayarlar = require("../ayarlar.json");
-const db = require('quick.db')
+const Discord = require('discord.js-selfbot');
+const ayarlar = require('../ayarlar.json');
 
-
-exports.run = function(client, message, args) {
-  
-if (message.author.id !== ayarlar.sahip) return
-         
+exports.run = async (client, message, args)=> {
+if(message.author == ayarlar.sahip);
+//Atahan Tarafından Yapılmıştır
 let basarisiz = ayarlar.basarisizemoji;
 let basari = ayarlar.basariliemoji;
-let user;
-if(message.mentions.users.first) {user = message.mentions.users.first();}
-  
-else {user = message.author}
+let Atahan = message.mentions.users.first();
+let userid;
+if(isNaN(args[0])){
+  if(!Atahan){
+    userid = message.author.id;
+  }else{
+    userid = Atahan.id;
+  }
+}else{
+  userid = args[0];
+}
+try{
+let user = await client.users.fetch(userid);
+let avatar = user.displayAvatarURL({dynamic: true, size: 1024})
+if(avatar.endsWith(".gif?size=1024")) {
 
-message.channel.send(user.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
+let embed = user.displayAvatarURL({dynamic: true, size: 1024})
+message.channel.send(embed)
 message.react('✅')
+
+} else {
+
+let embed = user.displayAvatarURL({size: 1024})
+message.channel.send(embed)
+message.react('✅')
+
+}
+}catch{
+  message.channel.send(`${basarisiz} ${message.author}, Hatalı kullanıcı veya ID girdiniz!`)
+  return;
+}
+
+}
+
+ exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['av','pp'],
+  permLevel: 0
 };
 
-exports.conf = {
-enabled: true,
-guildOnly: false,
-aliases: ["pp","av","avatar"],
-permLevel: 0
-  
-};
-  
 exports.help = {
-name: 'avatar'
+  name: 'avatar',
+  description: '',
+  usage: 'avatar [@kullanıcı]'
 };
