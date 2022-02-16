11f0438d04ebdda080587fc44b531c0b35491bd9
Glitch (atahan-selfbot) <none>
2022-02-16 20:41:01 +0000

    Rewound to commit 7f8527367d9e6970a9beeaa52a24e6ae836fa91d + 0 edits

---
+++ komutlar/sil.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js-selfbot');
+const database = require('quick.db');
+const ayarlar = require('../ayarlar.json')
+
+exports.run = async (client, message, args) => {
+
+if(message.author.id !== ayarlar.sahip) return
+  
+let basari = ayarlar.basariliemoji
+let basarisiz = ayarlar.basarisizemoji
+let channel = message.channel;
+let member = message.author
+
+if(!args[0]) return message.channel.send(`${basarisiz} ${message.author}, Bir sayı belirtmelisin!`)
+  
+var i = args[0];
+message.delete();
+channel.messages.fetch().then(x => {
+x.filter(a => a.author.id === member.id).map(a => a).slice(args[0]).forEach(s => {
+s.delete();
+if(i === x.filter(a => a.author.id === member.id).map(a => a).slice(args[0]).length) {
+return message.channel.send(`${basari} ${message.author}, **${i}** Mesaj siliniyor.`);
+}
+});
+});
+
+
+};
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "sil"
+};
