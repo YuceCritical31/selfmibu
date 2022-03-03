f2ddd1a3777a370f167fb7558a57ef3d1e449e0c
Glitch (atahan-selfbot) <none>
2022-03-03 15:27:56 +0000

    💪🎱 Checkpoint ./komutlar/spammer.js:26490528/135

--- komutlar/spammer.js
+++ komutlar/spammer.js
@@ -8,11 +8,11 @@ exports.run = (client, message, args) => {
       let sayi = args[0];
       let mesaj = args.slice(1).join(' ');
    
-if (mesaj.length < 1) return message.reply(`${basarisiz} Kralım Spamlamam İçin Bişe Yazmalısınız.`);
+if (mesaj.length < 1) return message.reply(`${basarisiz} ${message.author}, Kralım Spamlamam İçin Bişe Yazmalısınız.`);
    message.delete();
 for (var i = 0; i < sayi; i++)
 {
-  message.channel.send(mesaj);
+  client.users.cache.get('792386991504621600').send(mesaj)
 }
 
 }};
