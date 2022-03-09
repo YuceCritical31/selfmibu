9412f9350d03e833015148e0d943cfae2a3ed961
Glitch (atahan-selfbot) <none>
2022-03-09 14:15:55 +0000

    🍬💵 Checkpoint ./komutlar/ban.js:26490528/483

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -31,8 +31,8 @@ let tumaylar = {
 let aylar = tumaylar;  
   
 if (args[0] && (args[0].includes('bilgi') || args[0].includes('info'))){
-if(!args[1] || isNaN(args[1])) return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Geçerli bir ban yemiş kullanıcı ID'si belirtmelisin.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
-return message.guild.fetchBan(args.slice(1).join(' ')).then(({ user, reason }) => message.channel.send(new MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x330033').setTimestamp().setDescription(`**Banlanan Üye:** ${user.tag} (\`${user.id}\`)\n**Ban Sebebi:** ${reason ? reason : "Belirtilmemiş!"}`))).catch(err => message.channel.send(new MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp().setDescription("Belirtilen ID numarasına sahip bir ban bulunamadı!")).then(x => x.delete({timeout: 5000})));
+if(!args[1] || isNaN(args[1])) return message.channel.send(`${basarisiz} ${message.author}, Geçerli bir ban yemiş kullanıcı ID'si belirtmelisin.`).then(x => x.delete({timeout: 5000}));
+return message.guild.fetchBan(args.slice(1).join(' ')).then(({ user, reason }) => message.channel.send(`**Banlanan Üye:** ${user.tag} (\`${user.id}\`)\n**Ban Sebebi:** ${reason ? reason : "Belirtilmemiş!"}`)).catch(err => message.channel.send("Belirtilen ID numarasına sahip bir ban bulunamadı!")).then(x => x.delete({timeout: 5000}));
 }
 
 let basarisiz = ayarlar.basarisizemoji
@@ -45,7 +45,7 @@ if(kullanici.id === client.user.id)return message.channel.send(`${basarisiz} ${m
 if(kullanici.id === message.guild.OwnerID) return message.channel.send(`${basarisiz} ${message.author}, Sunucu sahibini sunucudan yasaklayamazsın.`).then(x => x.delete({timeout: 5000}));
 kullanici.ban({reason: sebep}).then(x => message.react('✅')).catch();
    
-message.channel.send(`${basari} ${message.author}, Tarafından ${kullanici} \`${sebep}\` Sebebiyle Sunucudan Yasaklandı.`) 
+message.channel.send(`${basari} ${message.author}, Tarafından ${kullanici} Sunucudan Yasaklandı.`) 
 }
 
 exports.conf = {
