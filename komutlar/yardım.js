6078df95794a036c090ed38ae12244dcb4ad5289
Glitch (atahan-selfbot) <none>
2022-03-13 10:36:49 +0000

    🐺💕 Checkpoint ./komutlar/yardım.js:26490528/59

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -15,24 +15,43 @@ exports.run = async(client, message, args) => {
 
 const extacy = `
 > **\`${prefix}ses Aç/Kapat -> Ses sitemini Açar/Kapatır\`**
+> 
 > **\`${prefix}ses-kanal <ses kanal id> -> Aktif oluncak ses kanalını ayarlar\`**
+> 
 > **\`${prefix}afk <sebep> -> Sizi afk moduna sokar\`**
+> 
 > **\`${prefix}unafk -> Sizi afk modundan çıkarır\`**
+> 
 > **\`${prefix}durum <durum> -> Özel durumunuzu değişir\`**
+> 
 > **\`${prefix}durum-sifirla -> Özel durumunuzu sıfırlar\`**
+> 
 > **\`${prefix}prefix <prefix> -> Prefixinizi değişir\`**
+> 
 > **\`${prefix}ping -> Botun pingini gösterir\`**
+> 
 > **\`${prefix}status <sayı> -> Botun durumunu belirler\`**
+> 
 > **\`${prefix}restart -> Botu yeniden başlatır\`**
+> 
 > **\`${prefix}dm-spam <kullanıcı etiketle/id gir> <sayı> <mesaj> -> Belirtilen kişinin dmsinde belirtilen mesajı belirtilen sayı ile spamlar\`**
+> 
 > **\`${prefix}spam <sayı> <mesaj> -> Belirtilen mesajı belirtilen sayı ile spamlar\`**
+> 
 > **\`${prefix}av -> Etiketlediğiniz veya id sini girdiğiniz kişinin avatarını görüntüliyebilirsiniz\`**
+> 
 > **\`${prefix}ban <@üye/ID> -> Etiketlediğiniz veya id sini girdiğiniz kişiyi banlar (ban yetkiniz varsa)\`**
+> 
 > **\`${prefix}kick <@üye/ID> -> Etiketlediğiniz veya id sini girdiğiniz kişiyi kickler (kick yetkiniz varsa)\`**
+> 
 > **\`${prefix}profil <@üye/ID> -> Etiketlediğiniz veya id sini girdiğiniz kişinin hesap bişgilerini gösterir\`**
+> 
 > **\`${prefix}tag-bul <tag> -> Belirtilen tagı sunucuda taratır ve kaç kişide olduğunu söyler\`**
+> 
 > **\`${prefix}snipe -> Sunucudaki son silinen mesajı görüntülersiniz\`**
+> 
 > **\`${prefix}sil <sayı> -> Belirtilen sayıda kendi mesajlarınızı siler\`**
+> 
 > **\`.unuttum -> Prefixi unuttuysanız bu komut ile prefixi görebilirsiniz\`**
 `
 message.channel.send(extacy)
