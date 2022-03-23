d0831e440a71b7154cac81c80808c242ed77c78c
Glitch (atahan-selfbot) <none>
2022-03-23 17:29:10 +0000

    💨🎏 Checkpoint ./atanis.js:26490528/2907 ./komutlar/yardım.js:26490528/4

--- atanis.js
+++ atanis.js
@@ -201,45 +201,7 @@ message.reply(`Prefix: \`${db.fetch(`prefix`)}\``)
 
 })
 
-client.on('message', async (message, member, guild) => {
-if (message.content === `${prefix}yardım`) {
-if (message.author.id !== ayarlar.sahip) return 
-const extacy = `
-> **\`${prefix}ses Aç/Kapat -> Ses sitemini Açar/Kapatır\`**
-> 
-> **\`${prefix}ses-kanal <ses kanal id> -> Aktif oluncak ses kanalını ayarlar\`**
-> 
-> **\`${prefix}afk <sebep> -> Sizi afk moduna sokar\`**
-> 
-> **\`${prefix}unafk -> Sizi afk modundan çıkarır\`**
-> 
-> **\`${prefix}durum <durum> -> Özel durumunuzu değişir\`**
-> 
-> **\`${prefix}durum-sifirla -> Özel durumunuzu sıfırlar\`**
-> 
-> **\`${prefix}prefix <prefix> -> Prefixinizi değişir\`**
-> 
-> **\`${prefix}ping -> Botun pingini gösterir\`**
-> 
-> **\`${prefix}status <sayı> -> Botun durumunu belirler\`**
-> 
-> **\`${prefix}restart -> Botu yeniden başlatır\`**
-> 
-> **\`${prefix}dm-spam <kullanıcı etiketle/id gir> <sayı> <mesaj> -> Belirtilen kişinin dmsinde belirtilen mesajı belirtilen sayı ile spamlar\`**
-> 
-> **\`${prefix}spam <sayı> <mesaj> -> Belirtilen mesajı belirtilen sayı ile spamlar\`**
-> 
-> **\`${prefix}av -> Etiketlediğiniz veya id sini girdiğiniz kişinin avatarını görüntüliyebilirsiniz\`**
-> 
-> **\`${prefix}ban <@üye/ID> -> Etiketlediğiniz veya id sini girdiğiniz kişiyi banlar (ban yetkiniz varsa)\`**
-> 
-> **\`${prefix}kick <@üye/ID> -> Etiketlediğiniz veya id sini girdiğiniz kişiyi kickler (kick yetkiniz varsa)\`**
-> 
-> **\`${prefix}profil <@üye/ID> -> Etiketlediğiniz veya id sini girdiğiniz kişinin hesap bişgilerini gösterir\`**
-> 
-> **\`${prefix}tag-bul <tag> -> Belirtilen tagı sunucuda taratır ve kaç kişide olduğunu söyler\`**}
 
-})
 
 client.on('message', async (msg, member, guild) => {
   
