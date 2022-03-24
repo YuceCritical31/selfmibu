a64ec17f55aee8290d839be7b8e04df41a7a2b7f
Glitch (atahan-selfbot) <none>
2022-03-24 18:22:11 +0000

    🐥🌹 Checkpoint ./komutlar/spammer.js:26490528/68 ./komutlar/aga-spammer.js:26490528/1398

--- komutlar/aga-spammer.js
+++ komutlar/aga-spammer.js
@@ -2,18 +2,27 @@ const Discord = require('discord.js-selfbot');
 const ayarlar = require('../ayarlar.json')
 ///spammer by planta
  
-exports.run = (client, message, args) => {
+exports.run = async(client, message, args) => {
   if (message.author.id === ayarlar.sahip) {
       let basarisiz = ayarlar.basarisizemoji
       let sayi = args[0];
       let mesaj = args.slice(1).join(' ');
 
-if (isNaN(sayi)) return message.channel.send(`${basarisiz} ${message.author}, .spam <sayı> <mesaj> şeklinde yazınız.`).then(x => x.delete({timeout: 5000}))
-if (mesaj.length < 1) return message.channel.send(`${basarisiz} ${message.author}, Kralım Spamlamam İçin Bişe Yazmalısınız.`).then(x => x.delete({timeout: 5000}))
+if (isNaN(sayi)) return message.channel.send(`${basarisiz} ${message.author}, .aga-spam <sayı> şeklinde yazınız.`).then(x => x.delete({timeout: 5000}))
    message.delete();
+    function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if (new Date().getTime() - start > milliseconds) {
+      break;
+    }
+  }
+}
 for (var i = 0; i < sayi; i++)
 {
-message.channel.send(mesaj)
+message.channel.send('aga u-4 al')
+await sleep(2000)
+message.channel.send('aga u-4 aç')
 }
 
 }};
@@ -26,7 +35,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'spam',
+  name: 'aga-spam',
   description: 'spammer',
   usage: 'spam [yazdırmak istediğiniz şey]'
 };
