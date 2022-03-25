6ef49a611ef24aa46a11e738ad7cc6cbb42407cb
Glitch (atahan-selfbot) <none>
2022-03-25 17:51:18 +0000

    🐏☺️ Checkpoint ./komutlar/aga-dm-spammer.js:26490528/905

---
+++ komutlar/aga-dm-spammer.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js-selfbot');
+const ayarlar = require('../ayarlar.json')
+///spammer by planta
+ 
+exports.run = async(client, message, args) => {
+  if (message.author.id === ayarlar.sahip) {
+      let basarisiz = ayarlar.basarisizemoji
+      let aga = client.users.cache.get("903919169068740658")
+      let sayi = args[0];
+      let mesaj = args.slice(1).join(' ');
+
+if (isNaN(sayi)) return message.channel.send(`${basarisiz} ${message.author}, .spam <sayı> <mesaj> şeklinde yazınız.`).then(x => x.delete({timeout: 5000}))
+message.delete();
+for (var i = 0; i < sayi; i++)
+{
+aga.send('aga u-4 al')
+aga.send('aga u-4 aç')
+}  
+}};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0,
+};
+
+exports.help = {
+  name: 'aga-dm-spam',
+  description: 'spammer',
+  usage: 'spam [yazdırmak istediğiniz şey]'
+};
