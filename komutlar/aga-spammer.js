ac34c64239a87cede3f0cccf55392bd120438ec2
Glitch (atahan-selfbot) <none>
2022-03-26 19:35:28 +0000

    🛋👢 Checkpoint ./komutlar/aga-spammer.js:26490528/12

--- komutlar/aga-spammer.js
+++ komutlar/aga-spammer.js
@@ -8,7 +8,7 @@ exports.run = async(client, message, args) => {
       let sayi = args[0];
       let mesaj = args.slice(1).join(' ');
 
-if (isNaN(sayi)) return message.channel.send(`${basarisiz} ${message.author}, .spam <sayı> <mesaj> şeklinde yazınız.`).then(x => x.delete({timeout: 5000}))
+if (isNaN(sayi)) return message.channel.send(`${basarisiz} ${message.author}, .aga-spam <sayı> şeklinde yazınız.`).then(x => x.delete({timeout: 5000}))
 message.delete();
 for (var i = 0; i < sayi; i++)
 {
