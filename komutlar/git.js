7810e91943730fd093917ffa082913acb67ada63
Glitch (atahan-selfbot) <none>
2022-03-29 17:56:44 +0000

    🍅⛸ Checkpoint ./komutlar/git.js:26490528/7503

--- komutlar/git.js
+++ komutlar/git.js
@@ -7,35 +7,17 @@ exports.run = async (client, message, emoji, args) => {
 if (message.author.id === ayarlar.sahip) {
     let basarili = ayarlar.basariliemoji;
     let basarisiz = ayarlar.basarisizemoji;
-  
-	let uye = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-  
+    let uye = message.mentions.members.first() || client.users.cache.get(args[0]);
+    let kanal = message.member.voice.setChannel(uye.voice.channelID)
+
   
   if (!uye) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} Ses odasına gidilecek üyeyi belirtmelisin!`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
-  if (!message.member.voice.channel || !uye.voice.channel || message.member.voice.channelID == uye.voice.channelID) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} İkiniz veya ikinizden birisi ses kanalında değil!`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
-  if (message.member.hasPermission("ADMINISTRATOR")) {await message.member.voice.setChannel(uye.voice.channelID);} else {
-   {
-    const reactionFilter = (reaction, user) => {
-      return ['✅'].includes(reaction.emoji.name) && user.id === uye.id;
-    };
-    message.channel.send({embed: new Discord.MessageEmbed().setColor('BLUE').setAuthor(uye.displayName, uye.user.avatarURL({dynamic: true, size: 2048})).setDescription(`${message.author} senin ses kanalına girmek için izin istiyor! Onaylıyor musun?`)}).then(async msj => {
-      await msj.react('✅');
-      msj.awaitReactions(reactionFilter, {max: 1, time: 15000, error: ['time']}).then(c => {
-	let cevap = c.first();
-	if (cevap) {
-	  message.member.voice.setChannel(uye.voice.channelID);
-          msj.delete();
-    let striga = new Discord.MessageEmbed()
-.setDescription(`${basarisiz} ${uye} Odaya Çekilme Teklifini Reddetti`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true }))
-    .setTimestamp().then(x => x.delete({timeout: 5000}));
-message.channel.send(striga)
-}
-})
-}
-      );
-    };
-  }};
+  if (!message.member.voice.channel || !uye.voice.channel || message.member.voice.channelID == uye.voice.channelID) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, İkiniz veya ikinizden birisi ses kanalında değil!`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+
+if (kanal) return
+  
   };
+};
 exports.conf = {
   enabled: true,
   guildOnly: true,
