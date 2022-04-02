b2690a0d8bffe898a02b76a97b9b890485ab1104
Glitch (atahan-selfbot) <none>
2022-04-02 12:06:09 +0000

    🐏🏙 Checkpoint ./komutlar/kes.js:26490528/1053

--- komutlar/kes.js
+++ komutlar/kes.js
@@ -1,21 +1,21 @@
 const Discord = require('discord.js-selfbot');
 const db = require('quick.db');
 const ayarlar = require('../ayarlar.json');
+let basarisiz = ayarlar.basarisizemoji
 
 exports.run = async (client, message, emoji, args) => {
   
 if (message.author.id === ayarlar.sahip) {
+if (!message.member.hasPermission("MOVE_MEMBERS")) return message.channel.send(`${basarisiz} ${message.author}, Kralım bu sunucuda \`ÜYELERİ TAŞI\` yetkiniz yok.`).then(x => x.delete({timeout: 5000}))
   
 
-let basarisiz = ayarlar.basarisizemoji
 let basari = ayarlar.basariliemoji
-let kanal = message.member.voiceChannel
-let member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
-if(!member) return;
-if(message.member.roles.highest.position <= member.roles.highest.position) return message.channel.send(`${basarisiz} ${message.author}, Etiketlenen kullanıcı sizden üst/aynı pozisyondadır.`).then(x => x.delete({timeout: 5000}));
-message.guild.member(member.id).voice.setChannel(null)
+let member = message.mentions.members.first() || client.users.cache.get(args[0])
+
+if(!member) return message.channel.send(`${basarisiz} ${message.author}, Ses bağlantısı kesilcek üyeyi belirtmelisin!`).then(x => x.delete({timeout: 5000}));
+
+member.voice.setChannel(null)
 message.react('✅')
-message.channel.send(`${basari} ${message.author}, ${member} Kullancısının ses bağlantısı kesildi.`).then(x => x.delete({timeout: 5000}));
 }}
 exports.conf = { 
 enabled: true, 
