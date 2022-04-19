4b20175407dd65cadb61a9dae3b848d1bfde7960
Glitch (atahan-selfbot) <none>
2022-04-19 14:12:00 +0000

    🎇💑 Checkpoint ./komutlar/taşı.js:26490528/142

--- komutlar/taşı.js
+++ komutlar/taşı.js
@@ -4,18 +4,18 @@ const ayarlar = require('../ayarlar.json');
 let basarili = ayarlar.basariliemoji;
 let basarisiz = ayarlar.basarisizemoji;
 
-exports.run = async (client, message, emoji, args) => {
+exports.run = async (client, message, args) => {
   
 if (message.author.id === ayarlar.sahip) {
   
 if (!message.member.hasPermission("MOVE_MEMBERS")) return message.channel.send(`${basarisiz} ${message.author}, Kralım bu sunucuda \`ÜYELERİ TAŞI\` yetkiniz yok.`).then(x => x.delete({timeout: 5000}))
   
-  let kanal = message.mentions.channels.first() || message.guild.channels.cache.get(args[1])
+  let kanal = message.mentions.channels.first()
   let uye = message.mentions.members.first() || message.guild.members.cache.get(args[0])
   let uye2 = message.mentions.members.first() || message.guild.members.cache.get(args[1])
   
   if (!uye) return message.channel.send(`${basarisiz} ${message.author}, Taşınacak üyeyi belirtmelisin!`).then(x => x.delete({timeout: 5000}));
-  //if (!args[1]) return message.channel.send(`${basarisiz} ${message.author}, Üyenin hangi kanala gidiceğini belirtmelisin!`).then(x => x.delete({timeout: 5000}));
+  if (!args[1]) return message.channel.send(`${basarisiz} ${message.author}, Üyenin hangi kanala gidiceğini belirtmelisin!`).then(x => x.delete({timeout: 5000}));
   //if (!uye2.voice.channel || !uye.voice.channel || uye.voice.channelID == uye.voice.channelID) return message.channel.send(`${basarisiz} ${message.author}, İkiniz veya ikinizden birisi ses kanalında değil!`).then(x => x.delete({timeout: 5000}));
 
 if (uye2) {
