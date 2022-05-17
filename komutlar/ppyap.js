89eb46811f1b5be5b9f96d7096de3625013efee7
Glitch (atahan-selfbot) <none>
2022-05-17 16:24:24 +0000

    ⛲️🛀 Checkpoint ./komutlar/ppyap.js:26490528/560

--- komutlar/ppyap.js
+++ komutlar/ppyap.js
@@ -7,16 +7,19 @@ if(message.author.id === ayarlar.sahip) {
 let basarisiz = ayarlar.basarisizemoji;
 let basari = ayarlar.basariliemoji;
 let Atahan = message.mentions.users.first() || client.users.cache.get(args[0])
+let link = [".webp",".png",".gif",".jpg"]
 
 if (!args[0]) return message.channel.send(`${basarisiz} ${message.author}, Bir kullanıcı veya bir link belirtmelisin.`).then(x => x.delete({timeout: 5000}))
-    
-let avatar = Atahan.displayAvatarURL({dynamic: true, size: 1024})
-if(args[0].includes([".webp",".png",".gif",".jpg"].some)) { avatar = args[0] }
+if (Atahan) { 
+client.user.setAvatar(Atahan.displayAvatarURL({dynamic: true, size: 1024})) 
+}
+  
+if (args[0].endsWith(link.some)) {
+  
+}
 
 message.react('✅')
-client.user.setAvatar(avatar)
 }}
-
  exports.conf = {
   enabled: true,
   guildOnly: false,
