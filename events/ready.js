c497ce16cda8629ac2481da32172c9830812f602
Glitch (atahan-selfbot) <none>
2022-05-21 04:49:34 +0000

    🎩☔️ Checkpoint ./events/ready.js:26490528/136 ./komutlar/status.js:26490528/18 ./komutlar/durum-süre.js:26490528/1611

--- events/ready.js
+++ events/ready.js
@@ -6,7 +6,8 @@ module.exports = client => {
   
   let şekil = db.fetch(`type`) || "PLAYING"
   let status = db.fetch(`status`) || "invisible"
+  let süre = db.fetch(`durum_süresi`) || null
   let durum = db.fetch(`durum`) || ayarlar.durum
   
-client.user.setPresence({ activity: { name: durum, type: şekil, timestamps: { start: Date.now(), }}, status: status})
+client.user.setPresence({ activity: { name: durum, type: şekil, timestamps: { start: süre, }}, status: status})
 }
