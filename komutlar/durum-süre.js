64368c5f48c5e267ee7498705f557952bffcea14
Glitch (atahan-selfbot) <none>
2022-05-21 05:13:34 +0000

    🛣🏚 Checkpoint ./komutlar/durum-süre.js:26490528/1342

--- komutlar/durum-süre.js
+++ komutlar/durum-süre.js
@@ -7,26 +7,36 @@ exports.run = async (client, message, args) => {
   
 if (message.author.id === ayarlar.sahip) {
 let sayılar = ["aç","kapat"]
-let data = db.fetch(`durum_süresi`)
+let data = await db.fetch(`durum`)
+let data2 = await db.fetch(`durum_süresi`)
 let status = args[0]
-if(!status) return message.channel.send(`${basarisiz} ${message.author}, Lütfen durumunuzu belirtiniz.\n1 = Çevrimiçi\n2 = Boşta\n3 = Rahatsız Etmeyin\n4 = Görünmez\n5 = Sıfırlamak`)
+if(!status) return message.channel.send(`${basarisiz} ${message.author}, ${db.fetch(`prefix`) || ayarlar.prefix}dz Aç/Kapat şeklinde yazınız.`).then(x => x.delete({timeout: 5000}))
 if(!sayılar.some(word => message.content.includes(word))) return message.channel.send(`${basarisiz} ${message.author}, Lütfen aşağıdaki sayılardan belirtiniz.\n1 = Çevrimiçi\n2 = Boşta\n3 = Rahatsız Etmeyin\n4 = Görünmez\n5 = Sıfırlamak`)
   
 if (status === "aç"){
+if (data2) return message.channel.send(`${basarisiz} ${message.author}, Durum zamanı zaten açık.`).then(x => x.delete({timeout: 5000}))
 await db.set(`durum_süresi`, Date.now())
-message.channel.send(`${basari} ${message.author}, Durum zamanınız kapatılıyor biraz bekleyin...`).then(msg => {
+if (!data){
+message.channel.send(`${basari} ${message.author}, Durum zamanınız açıldı.`)
+}
+if (data) {
+message.channel.send(`${basari} ${message.author}, Durum zamanınız açılıyor biraz bekleyin...`).then(msg => {
     console.log(`BOT: Yeniden Başlatılıyor.....`);
     process.exit(0);
-  })
+  })}
 message.react('✅')
 }
   
 if (status === "kapat"){
 await db.delete(`durum_süresi`)
+if (!data){
+message.channel.send(`${basari} ${message.author}, Durum zamanınız kapatıldı.`)
+}
+if (data) {
 message.channel.send(`${basari} ${message.author}, Durum zamanınız kapatılıyor biraz bekleyin...`).then(msg => {
     console.log(`BOT: Yeniden Başlatılıyor.....`);
     process.exit(0);
-  })
+  })}
 message.react('✅')
 }
 
