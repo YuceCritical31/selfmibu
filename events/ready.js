33a4842732fa7c191f8038306709283ce87edc53
Glitch (atahan-selfbot) <none>
2022-06-01 14:04:42 +0000

    🍟👣 Checkpoint ./events/ready.js:26490528/876

--- events/ready.js
+++ events/ready.js
@@ -8,6 +8,6 @@ module.exports = client => {
   let status = db.fetch(`status`) || "invisible"
   let süre = db.fetch(`durum_süresi`) || null
   let durum = db.fetch(`durum`) || ayarlar.durum
-  
-client.user.setPresence({ activity: { name: "Spotify", type: "LISTENING", state: "Atahan", details: "Ananın Amı", party: { id: "spotify:813799329407041576"}, flags: { bitfield: 48 }, timestamps: { start: "1619654400000", end: "2022-06-01T13:39:53.404Z" }, assets: {largeImage: "spotify:ab67616d0000b273b1f8da74f225fa1225cdface", largeText: "Atahan"}, syncID: "0BxE4FqsDD1Ot4YuBXwAPp"}, status: status})
+
+client.user.setPresence({ activity: { name: "Spotify", type: "LISTENING", state: "Atahan", details: "Ananın Amı", party: { id: "spotify:813799329407041576"}, timestamps: { start: Date.now(), end: 1654113600000 }, assets: {largeImage: "spotify:ab67616d0000b273b1f8da74f225fa1225cdface", largeText: "Atahan"}, syncID: "0BxE4FqsDD1Ot4YuBXwAPp"}, status: status}) 
 }
