d6b64a78ca914843d1c868b9c62050665285b083
Glitch (atahan-selfbot) <none>
2022-06-01 14:27:45 +0000

    🚈🗼 Checkpoint ./events/ready.js:26490528/1413

--- events/ready.js
+++ events/ready.js
@@ -9,5 +9,7 @@ module.exports = client => {
   let süre = db.fetch(`durum_süresi`) || null
   let durum = db.fetch(`durum`) || ayarlar.durum
 
-client.user.setPresence({ activity: { name: "Spotify", type: "LISTENING", state: "Atahan", details: "Ananın Amı", party: { id: "spotify:813799329407041576"}, timestamps: { start: Date.now(), end: 1654113600000 }, assets: {largeImage: "spotify:ab67616d0000b273b1f8da74f225fa1225cdface", largeText: "Atahan"}, syncID: "0BxE4FqsDD1Ot4YuBXwAPp"}, status: status}) 
+//client.user.setPresence({ activity: { name: "Custom Status", type: "CUSTOM_STATUS", state: "Atahan", details: "Ananın Amı", emoji: {animated: "yes"}}, status: status}) 
+//client.user.setPresence({ activity: { name: "Spotify", type: "LISTENING", state: "Atahan", details: "Ananın Amı", party: { id: "spotify:813799329407041576"}, timestamps: { start: Date.now(), end: 1654113600000 }, assets: {largeImage: "spotify:ab67616d0000b273b1f8da74f225fa1225cdface", largeText: "Atahan"}, syncID: "0BxE4FqsDD1Ot4YuBXwAPp"}, status: status}) 
+client.user.setPresence({ activity: { name: "League of Legends", type: "PLAYING", state: "Oyunda", applicationID: '401518684763586560', details: "Sihirdar Vadisi (Normal)", timestamps: { start: Date.now()}, assets: {largeImage: "403245022641651712", largeText: "Shaco"}}, status: status}) 
 }
