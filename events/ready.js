0c96027731cb54d2c4457308bc36c45fde232229
Glitch (atahan-selfbot) <none>
2022-06-02 10:36:38 +0000

    🔨👨 Checkpoint ./events/ready.js:26490528/495

--- events/ready.js
+++ events/ready.js
@@ -9,7 +9,16 @@ module.exports = client => {
   let süre = db.fetch(`durum_süresi`) || null
   let durum = db.fetch(`durum`) || ayarlar.durum
 
-//client.user.setPresence({ activity: { name: "Custom Status", type: "CUSTOM_STATUS", state: "Atahan", details: "Ananın Amı", emoji: {animated: "yes"}}, status: status}) 
+    var oyun = [
+    `deneme`,
+    `#kulakgözaajyok`
+]
+
+setInterval(() => {
+    var random = Math.floor(Math.random() * (oyun.length - 0 + 1));
+
+client.user.setPresence({ activity: { name: "Custom Status", type: "CUSTOM_STATUS", state: oyun[random]}, status: status}) 
+}, 5000)
 //client.user.setPresence({ activity: { name: "Spotify", type: "LISTENING", state: "Atahan", details: "Ananın Amı", party: { id: "spotify:813799329407041576"}, timestamps: { start: Date.now(), end: 1654113600000 }, assets: {largeImage: "spotify:ab67616d0000b273b1f8da74f225fa1225cdface", largeText: "Atahan"}, syncID: "0BxE4FqsDD1Ot4YuBXwAPp"}, status: status}) 
-client.user.setPresence({ activity: { name: "League of Legends", type: "PLAYING", state: "Oyunda", applicationID: "700136079562375258", details: "Sihirdar Vadisi (Normal)", timestamps: { start: Date.now()}, assets: {largeImage: "403245022641651712", largeText: "Shaco"}, createdTimestamp: 1654093203336}, status: status}) 
+//client.user.setPresence({ activity: { name: "League of Legends", type: "PLAYING", state: "Oyunda", applicationID: "700136079562375258", details: "Sihirdar Vadisi (Normal)", timestamps: { start: Date.now()}, assets: {largeImage: "403245022641651712", largeText: "Shaco"}, createdTimestamp: 1654093203336}, status: status}) 
 }
