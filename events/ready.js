00e853fb29bcf5cb9c560d4d8cf2b981e04d9893
Glitch (atahan-selfbot) <none>
2022-06-11 22:38:56 +0000

    🌻🚃 Checkpoint ./events/ready.js:26490528/71

--- events/ready.js
+++ events/ready.js
@@ -19,6 +19,6 @@ module.exports = client => {
 
 //client.user.setPresence({ activity: { name: "Custom Status", type: "CUSTOM_STATUS", state: oyun[random]}, status: status}) 
 //}, 5000)
-client.user.setPresence({ activity: { name: durum, type: şekil, timestamps: { start: süre }}, status: status}) 
+client.user.setPresence({ activity: { name: "Minecraft", type: "PLAYING", platform: "ios", timestamps: { start: Date.now() }}, status: status}) 
 //client.user.setPresence({ activity: { name: "League of Legends", type: "PLAYING", state: "Oyunda", applicationID: "700136079562375258", details: "Sihirdar Vadisi (Normal)", timestamps: { start: Date.now()}, assets: {largeImage: "403245022641651712", largeText: "Shaco"}, createdTimestamp: 1654093203336}, status: status}) 
 }
