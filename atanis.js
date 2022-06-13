eed71fb8e679484447119de76cba22ce84c44055
Glitch (atahan-selfbot) <none>
2022-06-13 00:16:38 +0000

    🎽👡 Checkpoint ./atanis.js:26490528/792

--- atanis.js
+++ atanis.js
@@ -47,10 +47,8 @@ var prefix = db.fetch(`prefix`) || ayarlar.prefix
 
 const log = message => {
     console.log(`${message}`);
-};
-
-snsnnsnsns
-
+}
+ 
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 fs.readdir('./komutlar/', (err, files) => {
