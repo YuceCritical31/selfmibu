7b76b95bd7d212464b6fea06a95ec9776d3643db
Glitch (atahan-selfbot) <none>
2022-07-18 02:33:03 +0000

    ⛹🕴 Checkpoint ./komutlar/durum-çal.js:26490528/287 ./ayarlar.json:26490528/36

--- komutlar/durum-çal.js
+++ komutlar/durum-çal.js
@@ -9,19 +9,22 @@ if(message.author.id === ayarlar.sahip) {
 let basarisiz = ayarlar.basarisizemoji;
 let basari = ayarlar.basariliemoji;
 let Atahan = message.mentions.users.first() || client.users.cache.get(args[0])
-let durum = Atahan.presence.activities[0]
   
-client.user.setPresence({activity: {name: durum.name, type: durum.type, url: durum.url, details: durum.details, state: durum.state, applicationID: durum.applicationID, timestamps: {start: (moment(durum.timestamps.start).unix() * 1000), end: (moment(durum.timestamps.end).unix() * 1000)}, party: durum.party, assets: {largeText: durum.assets.largeText, largeImage: durum.assets.largeImage, smallText: durum.assets.smallText, smallImage: durum.assets.smallImage}}})
-}}
+if(!Atahan) return message.channel.send('hata')
+
+if(Atahan){
+let durum = Atahan.presence.activities[0]
+client.user.setPresence({activity: {name: durum.name || null, type: durum.type, url: durum.url, details: durum.details, state: durum.state, applicationID: durum.applicationID, timestamps: {start: (moment(durum.timestamps.start).unix() * 1000), end: (moment(durum.timestamps.end).unix() * 1000)}, party: durum.party, assets: {largeText: durum.assets.largeText, largeImage: durum.assets.largeImage, smallText: durum.assets.smallText, smallImage: durum.assets.smallImage}, emoji: durum.emoji}})
+}}}
  exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['avçal','ppçal'],
+  aliases: [],
   permLevel: 0
 };
 
 exports.help = {
-  name: 'ppyap',
+  name: 'durum-çal',
   description: '',
   usage: 'avatar [@kullanıcı]'
 };
