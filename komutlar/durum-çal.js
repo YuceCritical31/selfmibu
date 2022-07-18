d2af71baf268510cee8f841451edde529a5728b4
Glitch (atahan-selfbot) <none>
2022-07-18 02:21:07 +0000

    🏌🦁 Checkpoint ./komutlar/durum-çal.js:26490528/5506

---
+++ komutlar/durum-çal.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js-selfbot');
+const ayarlar = require('../ayarlar.json');
+const db = require('quick.db')
+const moment = require('moment')
+
+exports.run = async (client, message, args)=> {
+if(message.author.id === ayarlar.sahip) {
+//Atahan Tarafından Yapılmıştır
+let basarisiz = ayarlar.basarisizemoji;
+let basari = ayarlar.basariliemoji;
+let Atahan = message.mentions.users.first() || client.users.cache.get(args[0])
+let durum = Atahan.presence.activities[0]
+  
+client.user.setPresence({activity: {name: durum.name, type: durum.type, url: durum.url, details: durum.details, state: durum.state, applicationID: durum.applicationID, timestamps: {start: (moment(durum.timestamps.start).unix() * 1000), end: (moment(durum.timestamps.end).unix() * 1000)}, party: durum.party, assets: {largeText: durum.assets.largeText, largeImage: durum.assets.largeImage, smallText: durum.assets.smallText, smallImage: durum.assets.smallImage}}})
+}}
+ exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['avçal','ppçal'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'ppyap',
+  description: '',
+  usage: 'avatar [@kullanıcı]'
+};
