9a10e332a065b06dfde8487bc8a7ed575ca675ad
Glitch (atahan-selfbot) <none>
2022-07-20 09:04:57 +0000

    🏯🍩 Checkpoint ./events/ready.js:26490528/2

--- events/ready.js
+++ events/ready.js
@@ -20,7 +20,7 @@ module.exports = client => {
 
 //client.user.setPresence({ activity: { name: "Custom Status", type: "CUSTOM_STATUS", state: oyun[random]}, status: status}) 
 //}, 5000)
-client.user.setPresence({ activity: {name: "Spotify", type: "LISTENING", details: "Another Love", state: "Tom Odell", timestamps: { start: Date.now(), end: Date.now() + ((4 * 60 * 1000) + (4 * 1000))}, party: {id:`spotify:${client.user.id}`}, assets: {largeText: "Long Way Down (Expanded Edition)", largeImage: "spotify:ab67616d0000b2739adb17697c5a5bb02b5cebb7"}, syncID: "5E4jBLx4P0UBji68bBThSw"}, status: status})
+//client.user.setPresence({ activity: {name: "Spotify", type: "LISTENING", details: "Another Love", state: "Tom Odell", timestamps: { start: Date.now(), end: Date.now() + ((4 * 60 * 1000) + (4 * 1000))}, party: {id:`spotify:${client.user.id}`}, assets: {largeText: "Long Way Down (Expanded Edition)", largeImage: "spotify:ab67616d0000b2739adb17697c5a5bb02b5cebb7"}, syncID: "5E4jBLx4P0UBji68bBThSw"}, status: status})
 //client.user.setPresence({ activity: { name: durum, type: şekil, timestamps: { start: süre }}, status: status}) 
 //client.user.setPresence({ activity: { name: "League of Legends", type: "PLAYING", state: "Oyunda", applicationID: "700136079562375258", details: "Sihirdar Vadisi (Normal)", timestamps: { start: Date.now()}, assets: {largeImage: "403245022641651712", largeText: "Shaco"}, createdTimestamp: 1654093203336}, status: status}) 
 }
